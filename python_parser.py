2f3aa1c04bfaa73c5ee28c81b60198dbcf5d4075
braedenconz <braedenltd@gmail.com>
2018-08-01 22:33:01 +1200

    added parsing python code into classes, and storing these classes as a object with class name and any super classes. Started functionality for adding attributes and functions to class object

--- python_parser.py
+++ python_parser.py
@@ -1,6 +1,21 @@
 import sys
 import inspect
 
+
+class ClassNode:
+    def __init__(self, name, super_classes):
+        self.name = name
+        self.attributes = []
+        self.functions = []
+        self.super_classes = super_classes
+
+    def add_attribute(self, attribute):
+        self.attributes.append(attribute)
+
+    def add_function(self, function):
+        self.functions.append(function)
+        
+
 class FileProcessor:
 
     def __init__(self):
@@ -21,6 +36,31 @@ class FileProcessor:
         __import__(module_name, locals(), globals())
 
         self.process_module(sys.modules[module_name])
+
+    def process_module(self, module):
+        """Find any classes that exists within this module"""
+        print("Processing module " + str(module))
+
+        for (name, something) in inspect.getmembers(module):
+            if inspect.isclass(something):
+                self.process_class(something)
+            else:
+                pass
+
+    def process_class(self, some_class):
+        """Process the found class, and store in global modules
+        Find any functions with-in the class"""
+        name = some_class.__name__
+
+        print("Processing class: " + name + " in module " + some_class.__module__)
+
+        module_name = some_class.__module__
+
+        if not module_name in self.modules:
+            self.modules[module_name] = list()
+
+        class_node = ClassNode(name, some_class.__bases__)
+        self.modules[module_name].append(class_node)
 
 
 if __name__ == "__main__":
