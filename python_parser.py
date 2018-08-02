aa9be2207d43e87758fb267cf5006fbca5d10f53
braedenconz <braedenltd@gmail.com>
2018-08-02 22:15:16 +1200

    Implemented graphviz dot outputting from python to UML, currently only outputs classes and their relationships

--- python_parser.py
+++ python_parser.py
@@ -2,6 +2,10 @@ import sys
 import inspect
 
 
+def log(string):
+    sys.stderr.write("LOG: " + string + "\n")
+
+
 class ClassNode:
     def __init__(self, name, super_classes):
         self.name = name
@@ -42,7 +46,7 @@ class FileProcessor:
     def process_file(self, file_name):
         """Import specified file_name and store as module"""
 
-        print("Processing " + file_name)
+        #print("Processing " + file_name)
 
         module_name = file_name.replace("./", "").replace(".py", "").replace("/", ".")
 
@@ -52,7 +56,7 @@ class FileProcessor:
 
     def process_module(self, module):
         """Find any classes that exists within this module"""
-        print("Processing module " + str(module))
+        #print("Processing module " + str(module))
 
         for (name, something) in inspect.getmembers(module):
             if inspect.isclass(something):
@@ -65,7 +69,7 @@ class FileProcessor:
         Find any functions with-in the class"""
         name = some_class.__name__
 
-        print("Processing class: " + name + " in module " + some_class.__module__)
+        #print("Processing class: " + name + " in module " + some_class.__module__)
 
         module_name = some_class.__module__
 
@@ -73,8 +77,18 @@ class FileProcessor:
         if not module_name in self.modules:
             self.modules[module_name] = list()
 
+        # strip bases
+        super_classes = []
+        super_classes_names = []
+
+        for x in some_class.__bases__:
+            if x.__name__ != 'object':
+                if x.__name__ not in super_classes_names:
+                    super_classes.append(x)
+                    super_classes_names.append(x.__name__)
+        
         # create class node and append to current module
-        class_node = ClassNode(name, some_class.__bases__)
+        class_node = ClassNode(name, super_classes)
         self.modules[module_name].append(class_node)
 
         # create list of attributes in class
@@ -89,25 +103,84 @@ class FileProcessor:
 
     def process_function(self, some_function, class_node):
         """Functions are added to the class node with just their title"""
-        print("Processing function: " + some_function.__name__, " - The parameters are:", inspect.getargspec(some_function)[0])
-
-        class_node.add_function(some_function, inspect.getargspec(some_function)[0])
+        #print("Processing function: " + some_function.__name__, " - The parameters are:", inspect.getargspec(some_function)[0])
+        class_node.add_function(some_function, inspect.getfullargspec(some_function)[0])
 
     def process_attribute(self, attribute_name, class_node):
         """Attributes are added to the class node with just their name"""
         # filter out __module__, __doc__
         if attribute_name not in self.filter_out_attributes:
-            print("Processing attribute: " + attribute_name)
+            #print("Processing attribute: " + attribute_name)
             class_node.add_attribute(attribute_name)
+
+    def output_to_dot(self, out):
+        """Output as UML class diagram using DOT (graphviz)"""
+        def line(s):
+            return out.write(s + "\n")
+
+        def class_name_to_dot(name):
+            return name
+
+        def write_row(out, method):
+            out.write(method + "\l")
+
+        out.write(
+            """
+            digraph G {
+                rankdir=BT
+                node [
+                    fontname = "Sans Not-Rotated 8"
+                    fontsize = 8
+                    shape = "record"
+                ]
+                edge [
+                    fontname = "Sans Not-Rotated 8"
+                    fontsize = 8
+                ]
+            """
+        )
+
+        for (name, module) in self.modules.items():
+            if len(module) > 1:
+                line("subgraph {")
+
+            for c in module:
+                line(class_name_to_dot(c.name) + " [")
+
+                # Class Title
+                out.write("label = \"{" + c.name)
+
+                out.write("}\"\n")
+
+                line("]")
+
+            if len(module) > 1:
+                line("}")
+
+        out.write("""
+            edge [
+                arrowhead = "empty"
+            ]
+        """)
+
+        for module in self.modules.values():
+            for c in module:
+                for parent in c.super_classes:
+                    line(class_name_to_dot(c.name) + " -> " +
+                         class_name_to_dot(parent.__name__))
+
+        line("}")
 
 
 if __name__ == "__main__":
     # USAGE: python_parser.py <filename or * for all>.py
+    # FOR OUTPUTTING DOT: | dot -T png -o output.png
 
     if len(sys.argv) == 1:
         print("USAGE: " + sys.argv[0] + " <pythonfiles>")
     else:
-        print("STARTING PROCESSING")
+        #print("STARTING PROCESSING")
 
         processor = FileProcessor()
         processor.process_files(sys.argv[1:])
+        processor.output_to_dot(sys.stdout)
