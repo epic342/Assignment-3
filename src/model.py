ce06b699ec3899635f9c13c810e91e9a4be4b787
mih0733 <michaelgoeswoof5@gmail.com>
2018-11-03 15:43:18 +1300

    Abstract Factory: Removed Abstract Factory

--- src/model.py
+++ src/model.py
@@ -1,12 +1,84 @@
 import inspect
 import os
 import sys
-from src.node_processor import ClassNode
 
 
 ##########################################
 # Initial helper classes to store information while the parser
 # parses the information
+
+
+class ClassNode:
+    """
+    Class object containing attributes and functions
+    Author: Braeden
+    Contributor: Peter
+
+    >>> ClassNode("Class One", []).name
+    'Class One'
+    >>> class_one = ClassNode("Class One")
+    >>> class_one.add_attribute("Attribute One", "+")
+    >>> class_one.add_attribute("Attribute Two", "+")
+    >>> len(class_one.attributes)
+    2
+    """
+
+    def __init__(self, name, super_classes=None):
+        self.name = name
+        self.attributes = []
+        self.functions = []
+        if super_classes is None:
+            self.super_classes = []
+        else:
+            self.super_classes = super_classes
+
+    def add_attribute(self, attribute_name, visibility):
+        self.attributes.append(AttributeNode(attribute_name, visibility))
+
+    def add_function(self, function_name, list_of_parameters, visibility):
+        self.functions.append(FunctionNode(function_name,
+                                           list_of_parameters, visibility))
+
+    def add_super_class(self, super_class):
+        self.super_classes.append(super_class)
+
+
+class AttributeNode:
+    """
+    Attribute object containing attribute name
+    Author: Braeden
+
+    >>> AttributeNode("Attribute One", "+").name
+    'Attribute One'
+    """
+
+    def __init__(self, name, visibility):
+        self.name = name
+        self.visibility = visibility
+
+
+class FunctionNode:
+    """
+    Function object containing function name and parameters
+    Author: Braeden
+
+    >>> FunctionNode("Function One", [], "+").get_name()
+    'Function One'
+    >>> len(FunctionNode("Function One",
+    ... ["Param One", "Param Two"], "+").parameters)
+    2
+    """
+
+    def __init__(self, name, list_of_parameters, visibility):
+        self.name = name
+        self.parameters = list_of_parameters
+        self.visibility = visibility
+
+    def get_name(self):
+        return self.name
+
+    def get_parameters(self):
+        return ",".join(self.parameters)
 
 
 class FileProcessor:
